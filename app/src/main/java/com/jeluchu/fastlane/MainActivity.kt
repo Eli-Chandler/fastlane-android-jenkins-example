27ec1e953b446ab3f26bfa6204ede70566225063
Jéluchu <infojeluchu@gmail.com>
2022-12-16 10:11:59 +0100

    Include Fastlane with Firebase

---
+++ app/src/main/java/com/jeluchu/fastlane/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.jeluchu.fastlane
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jeluchu.fastlane.ui.theme.Blue
+import com.jeluchu.fastlane.ui.theme.FastlaneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { FastlaneTheme { Info() } }
+    }
+}
+
+@Composable
+fun Info() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Blue),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            text = stringResource(id = R.string.extra_info),
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() { FastlaneTheme { Info() } }
